b8d198688e54213e7b3513ab248d2e8047b4ade0
shuaige <haijun13811581906@163.com>
2020-11-09 11:07:15 +0800

    修改頁面文案

--- src/base_config.js
+++ src/base_config.js
@@ -2,13 +2,13 @@ let BASE_URL = ''
 // const BASE_URL_DEVELOPMENT = ''
 // const BASE_URL_TEST = ''
 // const BASE_URL_PRODUCTION = ''
-// const BASE_URL_ALL = 'http://47.96.165.40:8188'
-// const CRM_URL = 'http://47.96.165.40:8080'
-const BASE_URL_ALL = 'http://127.0.0.1:8188'
+const BASE_URL_ALL = 'http://47.96.165.40:8188'
+const CRM_URL = 'http://47.96.165.40:8080'
+// const BASE_URL_ALL = 'http://127.0.0.1:8188'
 // const BASE_URL_ALL = 'http://127.0.0.1:8088'
-let CRM_URL = 'http://127.0.0.1:8181'
+// let CRM_URL = 'http://127.0.0.1:8181'
 
-const PROJ_KEY = 0
+const PROJ_INFO = 0
 
 switch (process.env.NODE_ENV) {
   case 'development':
@@ -29,5 +29,5 @@ export default {
   TITLE: '交易',
   BASE_URL,
   CRM_URL,
-  PROJ_KEY
+  PROJ_INFO
 }
