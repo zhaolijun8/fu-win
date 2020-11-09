5a3a95d0f84f78c4f42248220c1a4e83959bde85
zhaolijun8 <895878176@qq.com>
2020-11-09 11:20:35 +0800

    .

--- src/views/pages/trading_strategy/trading_strategy.vue
+++ src/views/pages/trading_strategy/trading_strategy.vue
@@ -11,7 +11,7 @@
       //- )
       TradingTab(
         :fee="filterFee",
-        :rate="filterProfitRate",
+        :broker="filterProfitRate",
         :propot="filterPropot"
         @filter="traderfilterHandler"
         @followFilter="followFilterHandler"
@@ -29,6 +29,7 @@
             :info="item",
             type="trading"
           )
+
         .trading-section
           .trade-title 交易英雄榜
             .trade-descript 数英雄人物还看今朝
@@ -36,32 +37,38 @@
           i.el-icon-close(@click="show = !show")
           .trading-name-txt 提示：如发现广告或虚假账号，或对账号统计数据有疑问请联系右侧客服QQ进行举报或处理。
         //- )
-        TradingZero(:subfee="subFee"
-          :acounttype="acountType"
-          @filter="acountfilterHandler")
+        TradingZero(
+            :subfee="subFee"
+            :acounttype="acountType"
+            @filter="subFeefilterHandler")
         .tranding-zero
           .tranding-zero-list
             .tranding-zero-list-item(v-for="(item, index) in heroList.list")
-              .avatar 
-                img(:src="item.url == '' ? avatar : item.url", width="100%")
+              .avatar
+                router-link(:to="{ path: '/trading_strategy/detail/'+item.signalId}")
+                    img(:src="item.avatarUrl == '' ? avatar : item.avatarUrl", width="100%")
                 .ranknum(:class="`color_${index + 1}`") {{ '0' + (index + 1) }}
-              .name {{item.name}}
+              .name {{item.signalName}}
               .yield-flex 收益率
-                span.txt {{item.yield}}
+                span.txt {{getPersent(item.orderIncomeRate)}}
               .accuracy-flex 准确率
-                span.txt {{item.accuracy}}
+                span.txt {{getPersent(item.orderWinRate)}}
               .profit-flex 获利
-                span.txt2 {{item.profit}}
-              .canvas-flex 
+                span.txt2 {{item.orderIncome}}
+              .canvas-flex
                 //- img(:src="item.canvasurl != '' ? item.canvasurl : canvasUrl")
                 tranding-veline(
                   :chartData="item.chartData"
                 )
-          el-pagination(
-            background
-            layout="prev, pager, next"
-            :total="heroList.totalNum"
-          )
+            el-pagination(
+                background
+                 @current-change="herofilterHandler"
+                layout="prev, pager, next"
+                :current-page="heroList.page.pageNo"
+                :total="heroList.page.total"
+                :page-size="heroList.page.pageSize"
+                :page-count="heroList.page.totalPages"
+              )
       div(v-show="tradingTab==1")
         trandingFollows(
           :followlist="followListData"
@@ -82,6 +89,8 @@ import E from "../../../utils";
 
 import avatar from '../../../assets/images/avatar-default.svg'
 import canvasUrl from '../../../assets/images/canvas-default.png'
+import commonRequest from "../../common/commonRequest";
+import commonAction from "../../common/commonAction";
 const filterLevel = [
   {
     label: "S",
@@ -130,6 +139,30 @@ const filterProfitRate = [
     label: "大于30",
     value: "0.3,1",
   },
+    {
+        label: "10-20",
+        value: "0.1,0.2",
+    },
+    {
+        label: "20-30",
+        value: "0.2,0.3",
+    },
+    {
+        label: "10-20",
+        value: "0.1,0.2",
+    },
+    {
+        label: "20-30",
+        value: "0.2,0.3",
+    },
+    {
+        label: "10-20",
+        value: "0.1,0.2",
+    },
+    {
+        label: "20-30",
+        value: "0.2,0.3",
+    },
 ];
 const filterFee = [
   {
@@ -143,20 +176,20 @@ const filterFee = [
 ];
 const filterPropot = [
   {
-    label: "小于10",
-    value: "0,0.1",
+    label: "小于15",
+    value: "0,0.15",
   },
   {
-    label: "10-50",
-    value: "0.1,0.5",
+    label: "15-30",
+    value: "0.15,0.3",
   },
   {
-    label: "50-100",
+    label: "30-50",
+    value: "0.3,0.5",
+  },
+  {
+    label: "50以上",
     value: "0.5,1",
-  },
-  {
-    label: "100以上",
-    value: "1",
   },
 ];
 const acountType = [
@@ -171,13 +204,13 @@ const acountType = [
 ];
 const subFee = [
   {
-    label: "获利金额",
-    value: "0",
-  },
-  {
     label: "准确率",
-    value: "1",
+    value: "orderWinRate",
   },
+    {
+        label: "获利金额",
+        value: "orderIncome",
+    },
 ];
 const jytype = [
   {
@@ -218,8 +251,11 @@ export default {
         pageNo: 1,
       },
       tradersRequest:{},
-      acountRequest:{},
-      followList: [],
+      followRequest:{
+          pageSize: 10,
+          pageNo: 1
+      },
+      followList: {},
       followRequest: {
         pageSize: 4,
         pageNo: 1,
@@ -237,45 +273,11 @@ export default {
       followType,
       filterBroker: [],
       show: true,
+      heroOrderBy: 'orderIncomeRate',
       heroList:{
-        list:[{
-          url:'',
-          name:'张三',
-          yield:'111.11%',
-          accuracy:'11.11%',
-          profit:'11.11',
-          chartData: {
-            columns: ["日期", "收益"],
-            rows: [
-              { "日期": "2020-10-01", "收益": "4.99"},
-              { "日期": "2020-10-02", "收益": "13.86"},
-              { "日期": "2020-10-03", "收益": "8" },
-              { "日期": "2020-10-04", "收益": "7"},
-              { "日期": "2020-10-05", "收益": "12"},
-              { "日期": "2020-10-06", "收益": "80"},
-              { "日期": "2020-10-07", "收益": "40"},
-            ],
-          },
-        },{
-          url:'',
-          name:'张三',
-          yield:'111.11%',
-          accuracy:'11.11%',
-          profit:'11.11',
-          chartData: {
-            columns: ["日期", "收益"],
-            rows: [
-              { "日期": "2020-10-01", "收益": "4.99"},
-              { "日期": "2020-10-02", "收益": "13.86"},
-              { "日期": "2020-10-03", "收益": "8" },
-              { "日期": "2020-10-04", "收益": "7"},
-              { "日期": "2020-10-05", "收益": "12"},
-              { "日期": "2020-10-06", "收益": "20"},
-              { "日期": "2020-10-07", "收益": "40"},
-            ],
-          },
-        }],
-        totalNum:100,
+        list:[],
+        page: {total: 0, totalPages: 0, pageNo: 0, pageSize: 0},
+        show:0
       },
       followListData:{
         list:[{
@@ -293,7 +295,7 @@ export default {
           week:'11.11',
           num:'12'
         }],
-        totalNum:100,
+        page: {total: 0, totalPages: 0, pageNo: 0, pageSize: 0}
       }
     };
   },
@@ -317,66 +319,69 @@ export default {
     this.getTradingList();
     this.getFollowList();
     this.getBrokerList();
+    this.getHeroList();
   },
   methods: {
     // 交易员列表
     getTradingList() {
-      let params = this.trandingRequest;
-      const storage = window.localStorage;
-      const projKey = storage.getItem("projKey");
-      if (projKey !== undefined && projKey !== null) {
-        params.projKey = projKey;
-      } else {
-        params.projKey = 0;
-      }
+      let params = this.trandingRequest
+        const storage = window.localStorage
+        const projInfo = storage.getItem('projInfo')
+        if (projInfo !== undefined && projInfo !== null) {
+            this.projInfo = JSON.parse(projInfo)
+            params.projKey = this.projInfo.projKey
+        }else {
+            params.projKey = 0
+        }
       let pageInfoHelper = {
         pageSize: 8,
-        pageNo: 1,
-      };
+        pageNo: 1
+      }
       let data = {
         params,
         pageInfoHelper,
-      };
-      return E.handleRequest(
-        E.api().post("signal/querySignalUsersPermit", data)
-      ).then((res) => {
+      }
+      commonRequest.querySignalUsersPermit(data,(res) => {
         this.tradingList = res.data.content.data;
-      });
+      })
     },
     // 跟随大师列表
-    getFollowList() {
-      let params = {};
-      const storage = window.localStorage;
-      const projKey = storage.getItem("projKey");
-      if (projKey !== undefined && projKey !== null) {
-        params.projKey = projKey;
-      } else {
-        params.projKey = 0;
-      }
+    getFollowList(pageNo) {
+        if(pageNo == undefined || pageNo ==null||pageNo==''){
+            pageNo=1
+        }
+        let params = this.followRequest
+        const storage = window.localStorage
+        const projInfo = storage.getItem('projInfo')
+        if (projInfo !== undefined && projInfo !== null) {
+            this.projInfo = JSON.parse(projInfo)
+            params.projKey = this.projInfo.projKey
+        }else {
+            params.projKey = 0
+        }
       let pageInfoHelper = {
-        pageSize: 4,
-        pageNo: 1,
+        pageSize: 10,
+        pageNo: pageNo,
       };
       let data = {
         params,
         pageInfoHelper,
       };
-      return E.handleRequest(
-        E.api().post("signal/queryProjectFollowUsers", data)
-      ).then((res) => {
-        this.followList = res.data.content.data;
+      commonRequest.queryFollowOrderSumPermit( data,(res) => {
+        this.followListData.list = res.data.content.data
+          this.followListData.page = res.data.page
       });
     },
     // 经销商列表
     getBrokerList() {
-      return E.handleRequest(E.api().post("comServer/queryBroker", {})).then(
-        (res) => {
+        commonRequest.queryBroker({},(res) => {
           this.brokerList = res.data.content.data;
           this.brokerList.forEach((item) => {
             const obj = {};
             obj.value = item.brokerName;
             obj.label = item.brokerName;
             this.filterBroker.push(obj);
+              this.filterProfitRate.push(obj);
           });
         }
       );
@@ -388,7 +393,7 @@ export default {
       let data = {
         params,
       };
-      E.handleRequest(E.api().post("admin/tokenLogin", data)).then((res) => {
+      commonRequest.tokenLogin(data,(res) => {
         if (res.data.status !== 0) {
           this.$message.warning(res.data.msg);
         } else {
@@ -403,25 +408,134 @@ export default {
         }
       });
     },
+      // 交易英雄榜
+      getHeroList(pageNo) {
+        if(pageNo == undefined || pageNo ==null||pageNo==''){
+            pageNo=1
+        }
+          let params = {}
+          const storage = window.localStorage
+          const projInfo = storage.getItem('projInfo')
+          if (projInfo !== undefined && projInfo !== null) {
+              this.projInfo = JSON.parse(projInfo)
+              params.projKey = this.projInfo.projKey
+          }else {
+              params.projKey = 0
+          }
+          params.orderBy=this.heroOrderBy
+          let pageInfoHelper = {
+              pageSize: 3,
+              pageNo: pageNo
+          }
+          let data = {
+              params,
+              pageInfoHelper
+          }
+          commonRequest.querySignalOrderSumPermit(data,res => {
+              if(res.data.content === null ||res.data.content===undefined ||res.data.content===''){
+                  this.$message.warning('无历史交易订单！')
+                  return
+              }
+              this.heroList.list = res.data.content.data
+              this.heroList.page = res.data.page
+              this.getProfitList(res.data.content.data)
+          })
+      },
+      //获取信号源近一个月的收益情况
+    getProfitList(contentData){
+        for(let i=0;i<contentData.length;i++){
+            let endDate = commonAction.getDay(new Date())
+            // let beginDate=commonAction.getPreMonth(endDate)
+            let beginDate="2019-01-01"
+            let params = {
+                userId: contentData[i].userId,
+                mtAccId: contentData[i].mtAccId,
+                tradeDate: [beginDate, endDate]
+            }
+            let pageInfoHelper = {
+                pageSize: 30,
+                pageNo: 1
+            }
+            let data = {
+                params,
+                pageInfoHelper
+            }
+            commonRequest.queryOrderFlow(data, res => {
+                if(res.data !== undefined&&res.data !== null&&res.data.content !== undefined&&res.data.content !== null){
+                    let result = res.data.content.data
+                    let income = 0
+                    for ( let a=0 ; a<result.length-1; a++) {
+                        income = income + result[a].orderIncome
+                        result[a+1].orderIncome = result[a+1].orderIncome + income
+                    }
+                    contentData[i].chartData = {}
+                    contentData[i].chartData.columns = ["tradeDate", "orderIncome"]
+                    contentData[i].chartData.rows = result
+                }
+            })
+        }
+        this.heroList.list = contentData
+        this.heroList.show = 1
+    },
+    getFollowFilterDate:function(value){
+        if(value==0){
+            return commonAction.getDateStr(-90)+','+commonAction.getDay(new Date())
+        }
+        if(value==1){
+            return commonAction.getDateStr(-180)+','+commonAction.getDay(new Date())
+        }
+        if(value==2){
+            return commonAction.getDateStr(-365)+','+commonAction.getDay(new Date())
+        }
+        if(value==3){
+            return commonAction.getDateStr(-730)+','+commonAction.getDay(new Date())
+        }
+        if(value==4){
+            return '2000-01-01,'+commonAction.getDateStr(-730)
+        }
+    },
+    getPersent: function(value) {
+        return commonAction.getPersent(value)
+    },
     // 筛选列表
     filterHandler(data) {
       this.trandingRequest = data;
       this.getTradingList();
     },
     traderfilterHandler(data) {
-      this.tradersRequest = data; 
-      //
-      // this.getTradingList();
-    },
-    acountfilterHandler(data){
-      this.acountRequest = data
+        this.trandingRequest = data;
+      this.getTradingList();
     },
     tradingTabHandler(data){
       this.tradingTab = data
     },
     followFilterHandler(data){
-      
-    }
+        if(data.groupBy==undefined||data.groupBy=={}||data.groupBy==null){
+            this.followRequest.orderBy = 'orderIncomeRate'
+        }else {
+            this.followRequest.orderBy = data.groupBy
+        }
+        if(data.tradeWeek!=undefined&&data.tradeWeek!={}&&data.tradeWeek!=null){
+            this.followRequest.beginDate = this.getFollowFilterDate(data.tradeWeek)
+        }else {
+            this.followRequest.beginDate = ''
+        }
+        this.getFollowList()
+    },
+      subFeefilterHandler(data){
+        if(data.subfee==undefined||data.subfee=={}||data.subfee==null){
+            this.heroOrderBy ='orderIncomeRate'
+        }else if(data.subfee!=undefined&& data.subfee!=null ){
+            this.heroOrderBy = data.subfee
+        }
+        this.getHeroList()
+      },
+      herofilterHandler(data){
+          this.getHeroList(data)
+      },
+      followOrderListHandler(data){
+          this.getFollowList(data)
+      }
 
   },
 };
@@ -498,15 +612,16 @@ export default {
       border-bottom: 1px solid #eee
       &:hover
         background: #F4F9FF
-      .txt 
+      &-link
+      .txt
         padding-left: 30px
-        font-size: 30px
+        font-size: 25px
         color: #333
         font-weight: bold
         vertical-align: -4px
       .txt2
         padding-left: 30px
-        font-size: 30px
+        font-size: 25px
         font-weight: bold
         color: #FB3F41
         vertical-align: -4px
